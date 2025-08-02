210e16205459d529fcc4ba634c65cbd2b5289d3d
PORIYA-GAUTAM <gautamporiya163@gmail.com>
2025-08-02 11:45:04 +0530

    feat: Update API endpoints to use production URLs for user and task management

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const callRegisterUserApi=async (formData)=>{
-    const response=await axios.post('http://localhost:5001/api/user/register',formData,{
+    const response=await axios.post('https://task-management-app-uwh9.onrender.com/api/user/register',formData,{
         withCredentials: true,
     })
 
@@ -10,7 +10,7 @@ export const callRegisterUserApi=async (formData)=>{
 }
 
 export const callLoginUserApi=async (formData)=>{
-    const response=await axios.post('http://localhost:5001/api/user/login',formData,{
+    const response=await axios.post('https://task-management-app-uwh9.onrender.com/api/user/login',formData,{
         withCredentials: true,
     })
 
@@ -18,36 +18,36 @@ export const callLoginUserApi=async (formData)=>{
 }
 
 export const callUserAuthApi=async ()=>{
-    const response=await axios.post('http://localhost:5001/api/user/auth',{},{
+    const response=await axios.post('https://task-management-app-uwh9.onrender.com/api/user/auth',{},{
          withCredentials: true,
     })
     return response?.data;
 }
 
 export const callLogOutApi=async()=>{
-    const response =await axios.post('http://localhost:5001/api/user/logout',{},{
+    const response =await axios.post('https://task-management-app-uwh9.onrender.com/api/user/logout',{},{
         withCredentials: true,
     })
     return response?.data;
 };
 
 export const addNewTaskApi=async (formData) => {
-    const response=await axios.post('http://localhost:5001/api/task/add-new-task',formData)
+    const response=await axios.post('https://task-management-app-uwh9.onrender.com/api/task/add-new-task',formData)
     return response?.data;
 }
 export const getAllTaskApi=async(getCurrentUserId)=>{
     console.log(getCurrentUserId,"user id from fronted")
-     const response=await axios.get(`http://localhost:5001/api/task/get-all-tasks-by-userid/${getCurrentUserId}`)
+     const response=await axios.get(`https://task-management-app-uwh9.onrender.com/api/task/get-all-tasks-by-userid/${getCurrentUserId}`)
      console.log("response of all Task",response)
     return response?.data;
 }
 export const updateTaskApi=async(formData)=>{
-     const response=await axios.put(`http://localhost:5001/api/task/update-task`,formData)
+     const response=await axios.put(`https://task-management-app-uwh9.onrender.com/api/task/update-task`,formData)
      console.log("response of Update Task",response)
     return response?.data;
 }
 export const deleteTaskApi=async(getCurrentUserId)=>{
-     const response=await axios.delete(`http://localhost:5001/api/task/delete-task/${getCurrentUserId}`)
+     const response=await axios.delete(`https://task-management-app-uwh9.onrender.com/api/task/delete-task/${getCurrentUserId}`)
      console.log("response of delete Task",response)
     return response?.data;
 }
