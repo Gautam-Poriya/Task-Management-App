1488dbb4f8bcca79932cc80f21443ce99b35566d
Gautam-Poriya <158160264+Gautam-Poriya@users.noreply.github.com>
2025-08-15 17:26:47 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ const app = express();
 // app.use(cors());
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://task-management-app-frotend.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
